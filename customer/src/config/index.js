b14be1931ea96f2da2d85d0f117c05dfdeb7fcc4
notaskhalsa <askhalsa29@gmail.com>
2025-03-12 04:24:21 -0400

    fix: change db config

--- customer/src/config/index.js
+++ customer/src/config/index.js
@@ -1,16 +1,9 @@
 const dotEnv = require("dotenv");
+dotEnv.config();
 
-
-if (process.env.NODE_ENV !== "prod") {
-  const configFile = `./.env.${process.env.NODE_ENV}`;
-  // console.log(process.env)
-  dotEnv.config({ path: configFile });
-} else {
-  dotEnv.config();
-}
 
 module.exports = {
   PORT: 8001,
-  DB_URL:"mongodb+srv://askhalsa29:HJrXaTovAu6ykXWB@products.5smyjhy.mongodb.net/",
+  DB_URL: process.env.DB_URL,
   APP_SECRET: 'jg_youtube_tutorial',
 };
